fc623db6a64f84afbe7a742e380e342afa7a86b8
chads <69518187+chads95@users.noreply.github.com>
2020-08-26 22:25:30 +0100

    Create NthPrime
    Added project

---
+++ NthPrime/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NthPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NthPrime(15));
+        }
+
+        public static bool CheckPrime(int number)
+        {
+            for (var i = 2; i < number; i++)
+            {
+                float divided = (float)number / i;
+
+                if(divided % 1 == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static int NthPrime(int prime)
+        {
+            byte conuter = 0;
+            for(var i =0; i <= 100; i++)
+            {
+                if(CheckPrime(i))
+                {
+                    conuter++;
+                }
+
+                if(conuter == prime)
+                {
+                    return (i);
+                }
+            }
+            return 0;
+        }
+    }
+}
